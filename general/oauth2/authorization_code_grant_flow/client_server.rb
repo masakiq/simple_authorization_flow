97e3d8bcfb19223e243d990d7931ab4d8a0abe6d
maeda1150 <masaki19770108@gmail.com>
2020-02-16 22:28:44 +0900

    Use state in auth flow
    modified:   auth_server.rb
    modified:   client_server.rb

--- general/oauth2/authorization_code_grant_flow/client_server.rb
+++ general/oauth2/authorization_code_grant_flow/client_server.rb
@@ -1,5 +1,6 @@
 require 'webrick'
 require 'net/http'
+require 'securerandom'
 
 client_port = ENV['SAF_CLIENT_SERVER_URI'].match(/\Ahttp:\/\/localhost:(?<port>.+?)\z/)[:port].to_i
 server = WEBrick::HTTPServer.new :Port => client_port
@@ -8,11 +9,13 @@ class Root < WEBrick::HTTPServlet::AbstractServlet
   def do_GET request, response
     client_id = ENV['SAF_CLIENT_ID']
     callback = "#{ENV['SAF_CLIENT_SERVER_URI']}/callback"
+    $state = SecureRandom.urlsafe_base64(10)
     location =
-      "#{ENV['SAF_AUTH_SERVER_URI']}/authorization?"\
-      'response_type=code&'\
-      "client_id=#{client_id}&"\
-      "redirect_uri=#{callback}"
+      "#{ENV['SAF_AUTH_SERVER_URI']}/authorization"\
+      '?response_type=code'\
+      "&client_id=#{client_id}"\
+      "&redirect_uri=#{callback}"\
+      "&state=#{$state}"
 
     response.status = 200
     response['Content-Type'] = 'text/html'
@@ -26,25 +29,27 @@ class Callback < WEBrick::HTTPServlet::AbstractServlet
   def do_GET request, response
     callback = "#{ENV['SAF_CLIENT_SERVER_URI']}/callback"
     code = request.query['code']
+    state = request.query['state']
 
-    if code&.size.to_i > 0
-      token_uri =
-        "#{ENV['SAF_AUTH_SERVER_URI']}/token"\
-        "?grant_type=authorization_code"\
-        "&code=#{code}"\
-        "&redirect_uri=#{callback}"
-      res = Net::HTTP.get_response(URI.parse(token_uri)).body
-      access_token = res.split(',').first.match(/access_token:(?<token>.+)/)[:token]
+    response.status = 302
 
-      user_info_uri = "#{ENV['SAF_RESOURCE_SERVER_URI']}/user_info?access_token=#{access_token}"
-      user_info = Net::HTTP.get_response(URI.parse(user_info_uri)).body
-
-      response.status = 302
-      response['Location'] = "/finish?user_info=#{user_info}"
-    else
-      response.status = 302
+    if $state.nil? || $state != state || code&.size.to_i == 0
       response['Location'] = '/finish?authorization=failed'
+      return
     end
+
+    token_uri =
+      "#{ENV['SAF_AUTH_SERVER_URI']}/token"\
+      "?grant_type=authorization_code"\
+      "&code=#{code}"\
+      "&redirect_uri=#{callback}"
+    res = Net::HTTP.get_response(URI.parse(token_uri)).body
+    access_token = res.split(',').first.match(/access_token:(?<token>.+)/)[:token]
+
+    user_info_uri = "#{ENV['SAF_RESOURCE_SERVER_URI']}/user_info?access_token=#{access_token}"
+    user_info = Net::HTTP.get_response(URI.parse(user_info_uri)).body
+
+    response['Location'] = "/finish?user_info=#{user_info}"
   end
 end
 
